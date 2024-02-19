71c07a1ef0364a52f1ef93015ad5cea0fa919083
SaranaDheeeraj <dheeraj.20bcn7055@vitap.ac.in>
2024-02-20 00:22:34 +0530

    done with all the changes

--- frontend/src/components/SingleChat.jsx
+++ frontend/src/components/SingleChat.jsx
@@ -22,7 +22,7 @@ import "./styles.css";
 import ScrollableChat from "./ScrollableChat";
 import io from "socket.io-client";
 
-const ENDPOINT = "http://localhost:5000";
+const ENDPOINT = "/";
 var socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
